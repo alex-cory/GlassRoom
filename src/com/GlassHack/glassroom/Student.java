769e5bce78a6ac41f48133993427b329b273d93d
Saleh ElHattab <saelhatt@indiana.edu>
2014-06-21 08:37:55 -0700

    Added David and Matt's ish

---
+++ src/com/GlassHack/glassroom/Student.java
@@ -0,0 +1,81 @@
+package com.GlassHack.glassroom;
+
+public class Student {
+	private String name;
+	private int id;
+	private int correctEnglish;
+	private int incorrectEnglish;
+	private int correctMath;
+	private int incorrectMath;
+	private int correctScience;
+	private int incorrectScience;
+	
+	public Student(String name, int id){
+		this.setName(name);
+		this.setId(id);
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getCorrectEnglish() {
+		return correctEnglish;
+	}
+
+	public void setCorrectEnglish(int correctEnglish) {
+		this.correctEnglish = correctEnglish;
+	}
+
+	public int getEnglish() {
+		return incorrectEnglish;
+	}
+
+	public void setEnglish(int incorrectEnglish) {
+		this.incorrectEnglish = incorrectEnglish;
+	}
+
+	public int getCorrectMath() {
+		return correctMath;
+	}
+
+	public void setCorrectMath(int correctMath) {
+		this.correctMath = correctMath;
+	}
+
+	public int getMath() {
+		return incorrectMath;
+	}
+
+	public void setMath(int incorrectMath) {
+		this.incorrectMath = incorrectMath;
+	}
+
+	public int getCorrectScience() {
+		return correctScience;
+	}
+
+	public void setCorrectScience(int correctScience) {
+		this.correctScience = correctScience;
+	}
+
+	public int getScience() {
+		return incorrectScience;
+	}
+
+	public void setScience(int incorrectScience) {
+		this.incorrectScience = incorrectScience;
+	}
+}
